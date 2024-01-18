a642e39c547eb00be40847ff1d0c0c299b092d52
Dennis <dennistgb579@gmail.com>
2024-01-18 21:53:27 +0800

    Update carrrr.ino

--- carrrr/carrrr.ino
+++ carrrr/carrrr.ino
@@ -4,48 +4,48 @@
 #define r2 5
 #define IR1Pin  A0
 #define IR2Pin  A1
-int IR1_Val = 0;
-void Full_speed_forward(void);
-void Full_speed_backward(void);
+
 void forwards(int speed);
-void backwards(int speed);
 void turnright();
 void turnleft();
 void brake(void);
+void line_trace();
+
 void setup() {
     Serial.begin(9600);
     pinMode(l1, OUTPUT);
     pinMode(l2, OUTPUT);
     pinMode(r1, OUTPUT);
     pinMode(r2, OUTPUT);
-    
+
 }
 void loop() {
+    int is_bed_there = 0;
+    while (is_bed_there == 0) {
+        line_trace();
+    }
+    
+}
+void line_trace() {
     int lightVal1 = analogRead(IR1Pin);
     int lightVal2 = analogRead(IR2Pin);
-    
-    Serial.print(lightVal1);
-    Serial.print(",");
-    Serial.print(lightVal2);
-    Serial.print("\n");
-    if (lightVal1 < 435){
-      if (lightVal2 < 435){
-        forwards(85);
-      }
-      else{
-        turnright();
-      }
+    if (lightVal1 < 435) {
+        if (lightVal2 < 435) {
+            forwards(85);
+        }
+        else {
+            turnright();
+        }
     }
-    else{
-      if (lightVal2 > 435){
-        forwards(30);
-      }
-      else{
-        turnleft();
-      }
+    else {
+        if (lightVal2 > 435) {
+            forwards(30);
+        }
+        else {
+            turnleft();
+        }
     }
 }
-
 void forwards(int speed) {
     digitalWrite(r2, LOW);
     analogWrite(r1, speed);
@@ -59,15 +59,15 @@ void brake() {
     digitalWrite(r1, LOW);
     digitalWrite(r2, LOW);
 }
-void turnright(){
-  analogWrite(r1, 100);
-  digitalWrite(r2, LOW);
-  digitalWrite(l1, LOW);
-  digitalWrite(l2, LOW);
+void turnright() {
+    analogWrite(r1, 100);
+    digitalWrite(r2, LOW);
+    digitalWrite(l1, LOW);
+    digitalWrite(l2, LOW);
 }
-void turnleft(){
-  digitalWrite(r1, LOW);
-  digitalWrite(r2, LOW);
-  analogWrite(l1, 110);
-  digitalWrite(l2, LOW);
+void turnleft() {
+    digitalWrite(r1, LOW);
+    digitalWrite(r2, LOW);
+    analogWrite(l1, 110);
+    digitalWrite(l2, LOW);
 }
